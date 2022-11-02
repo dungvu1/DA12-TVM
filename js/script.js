0b29496dc9d5e75cb360fcb2017f724ec4fe0357
Vu Duc Dung <dungvulicht@gmail.com>
2022-11-02 18:32:56 +0700

    update -

--- js/script.js
+++ js/script.js
@@ -5,11 +5,20 @@ $(document).ready(function(){
     //     slidesToShow: 3,
     //     slidesToScroll:2
     //   });
-
+// if($('.container')=100%){
 $('.regular').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 1000,
+  autoplaySpeed: 500,
 });
+// };
+// else($('.container')<= 50%){
+//   $('.regular').slick({
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 500,
+//   });
+// };
 });
