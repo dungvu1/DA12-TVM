7e5117670372538da936d464d9a1a557e03eb8b9
Vu Duc Dung <dungvulicht@gmail.com>
2022-11-05 01:12:18 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
-    // $(".regular").slick({
-    //     dots: false,
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll:2
-    //   });
-// if($('.container')=100%){
-$('.regular').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 500,
+// $('.regular').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 500,
+// });
+  $('.regular').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 500,
+        },
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 500,
+        }
+      }
+    ]
+  });
 });
-// };
-// else($('.container')<= 50%){
-//   $('.regular').slick({
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 500,
-//   });
-// };
-});
+
